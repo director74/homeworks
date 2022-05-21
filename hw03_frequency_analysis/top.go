5f1a69c4c10d0996e880fbfd61c083cacb1b0c62
Alexandr <alexandr.chel@mail.ru>
2022-05-21 22:02:30 +0500

    HW3 in process

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	var topSlice []string
+
+	type textPart struct {
+		word string
+		cnt  int
+	}
+
+	dict := make(map[string]int)
+
+	for _, str := range strings.Fields(text) {
+		dict[str] += 1
+	}
+
+	result := make([]textPart, 0)
+
+	for word, cnt := range dict {
+		result = append(result, textPart{word: word, cnt: cnt})
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].cnt > result[j].cnt || result[i].cnt == result[j].cnt && strings.Compare(result[i].word, result[j].word) < 0
+	})
+
+	j := len(result)
+	for i := 0; i < 10 && j > 0; i++ {
+		topSlice = append(topSlice, result[i].word)
+		j--
+	}
+
+	return topSlice
 }
