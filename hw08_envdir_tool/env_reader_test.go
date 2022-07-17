2a36853aee5b1fc69e762a1a3ece2a5593e14d9c
alexandr <alexandr.chel@mail.ru>
2022-07-18 01:20:36 +0500

    HW8 tests

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,37 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"github.com/stretchr/testify/require"
+	"io/fs"
+	"os"
+	"testing"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("wrong directory", func(t *testing.T) {
+		_, err := ReadDir("testdata/envv")
+
+		require.True(t, errors.Is(err, fs.ErrNotExist))
+	})
+
+	t.Run("empty directory path", func(t *testing.T) {
+		_, err := ReadDir("")
+
+		require.Equal(t, err, ErrEmptyDirPath)
+	})
+
+	t.Run("env name with =", func(t *testing.T) {
+		dir, _ := os.MkdirTemp("/tmp", "env_test")
+		badFile, errTmpFile := os.CreateTemp(dir, "WORLD=")
+
+		_, err := ReadDir(dir)
+
+		os.Remove(badFile.Name())
+		errDirRemove := os.Remove(dir)
+
+		require.NoError(t, errTmpFile)
+		require.NoError(t, errDirRemove)
+		require.Equal(t, err, ErrForbiddenFileSymbols)
+	})
 }
