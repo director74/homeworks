1606eccb93402facde28cdf2886a6ee2e4588b8d
alexandr <alexandr.chel@mail.ru>
2022-07-17 23:04:16 +0500

    HW8 in process

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+	eCom := exec.Command(cmd[0], cmd[1:]...)
+	eCom.Stdin = os.Stdin
+	eCom.Stdout = os.Stdout
+	eCom.Stderr = os.Stderr
+
+	for key, envItem := range env {
+		if envItem.NeedRemove {
+			os.Unsetenv(key)
+		} else {
+			os.Setenv(key, envItem.Value)
+		}
+	}
+
+	eCom.Env = append(os.Environ())
+
+	if err := eCom.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return exitError.ExitCode()
+		}
+	}
+
 	return
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,36 +1,26 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
 
-type args struct {
-	dirPath string
-	arg1    string
-	arg2    string
-}
-
-func processArgs(params []string) args {
-	result := args{}
-
-	switch len(params) {
-	case 2:
-		result.dirPath = params[1]
-	case 3:
-		result.arg1 = params[2]
-	case 4:
-		result.arg2 = params[3]
-	}
-
-	return result
-}
+var ErrNotEnoughArguments = errors.New("not enough few arguments passed")
 
 func main() {
-	args := processArgs(os.Args)
+	var dirPath string
 
-	_, err := ReadDir(args.dirPath)
+	if len(os.Args) < 2 {
+		fmt.Println(ErrNotEnoughArguments)
+		return
+	}
+	dirPath = os.Args[1]
+
+	envs, err := ReadDir(dirPath)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+	RunCmd(os.Args[2:], envs)
 }
