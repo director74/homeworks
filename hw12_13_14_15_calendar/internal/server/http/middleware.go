b809dd2ddbf79b49764b08a1c71f2e62b7d27b0d
alexandr <alexandr.chel@mail.ru>
2022-08-28 12:13:24 +0500

    hw12 calendar app first realisation

--- hw12_13_14_15_calendar/internal/server/http/middleware.go
+++ hw12_13_14_15_calendar/internal/server/http/middleware.go
@@ -1,11 +1,44 @@
 package internalhttp
 
 import (
+	"log"
+	"net"
 	"net/http"
+	"time"
+
+	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/storage"
 )
 
-func loggingMiddleware(next http.Handler) http.Handler {
+type statusWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *statusWriter) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
+func loggingMiddleware(next http.Handler, logg Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO
+		start := time.Now()
+		sw := statusWriter{ResponseWriter: w}
+		next.ServeHTTP(&sw, r)
+		rqDuration := time.Since(start).Seconds()
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			log.Fatalf("userip: %q is not IP:port", r.RemoteAddr)
+		}
+
+		logg.Infof("%s [%s] %s %s %s %d %f \"%s\"\n",
+			ip,
+			start.Format(storage.LayoutLog),
+			r.Method,
+			r.RequestURI,
+			r.Proto,
+			sw.status,
+			rqDuration,
+			r.UserAgent(),
+		)
 	})
 }
--- hw12_13_14_15_calendar/internal/storage/event.go
+++ hw12_13_14_15_calendar/internal/storage/event.go
@@ -1,7 +1,29 @@
 package storage
 
+import (
+	"database/sql"
+	"errors"
+	"time"
+)
+
+var (
+	ErrDateBusy       = errors.New("selected time already reserved by another event")
+	ErrWrongDateStart = errors.New("event begin date can't be in past")
+	ErrWrongDateEnd   = errors.New("event finish can't be same or earlier then start")
+	ErrWrongTitle     = errors.New("event title can't be empty")
+)
+
+const (
+	DateFormatISO = "2006-01-02"
+	LayoutLog     = "01/Jan/2006 03:04:05 -0700"
+)
+
 type Event struct {
-	ID    string
-	Title string
-	// TODO
+	ID                   int64          `db:"ID" fake:"{number:1,9223372036854775807}"`
+	Title                string         `db:"Title" fake:"{sentence:10}"`
+	DateStart            time.Time      `db:"DateStart"`
+	DateEnd              time.Time      `db:"DateEnd"`
+	Description          sql.NullString `db:"Description"`
+	UserID               int64          `db:"UserID" fake:"{number:1,9223372036854775807}"`
+	NotificationInterval sql.NullInt16  `db:"NotificationInterval"`
 }
